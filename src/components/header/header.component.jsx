a63a3bd38af76b91bcff9625b0e0386898c530e0
longp2995 <longp2995@gmail.com>
2019-10-30 22:19:42 -0700

    added redux, and implemented userReducer and userActions to Header and App Components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.styles.scss';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 
 const Header = ({currentUser}) => (
@@ -27,6 +28,10 @@ const Header = ({currentUser}) => (
 		</div>
 	</div>
 
-)
+);
 
-export default Header;
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
 import './index.css';
 import App from './App';
 
+import store from './redux/store';
+
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>, 
+	// Provider is a component that is the parent of everything inside of the application
+	// It allows us to get access to all of the things realted to the store
+	<Provider store={store}>   
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>, 
 	document.getElementById('root')
 
 );
